4d1d3fd6f5106e9c0344c2fd4e1a290b3bb5e331
4it brainiac <forit_brainiacs@gmail.com>
2016-07-05 17:03:29 +0300

    Card Page

---
+++ flashcards/Modules/CardSetModule.cs
@@ -0,0 +1,20 @@
+﻿using Nancy;
+
+namespace flashcards.Modules
+{
+	public class CardSetModule : NancyModule
+	{
+		public CardSetModule() : base("/cardset")
+		{
+			Get["/learn"] = parameters =>
+			{
+				return View["learn"];
+			};
+
+			//Post["/learn"] = parameters =>
+			//{
+			//	return View["learn"];
+			//};
+		}
+	}
+}
